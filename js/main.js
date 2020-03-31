f8421b9dcb43ba22e580296fcef64bebbeda1444
vcunningham <wolf1023@gmail.com>
2020-03-30 20:32:04 -0500

    Activity 9
    I didn't use a second topo file or graticule so it looks very plain

--- js/main.js
+++ js/main.js
@@ -1,149 +1,49 @@
-window.onload = function(){
-	var w = 900, h = 500;
+//begin script when window loads
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
+	 //map frame dimensions
+    var width = 800,
+        height = 800;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+		
+	var projection = d3.geoAlbers()
+		.center([0.00, 31.78])
+		.rotate([90.09, -11.82, 0])
+		.parallels([45.00, 90.00])
+		.scale(4114.14)
+		.translate([width / 2, height / 2]);
+		
+	var path = d3.geoPath()
+        .projection(projection);
+
 	
-	var container = d3.select("body")
-		.append("svg")
-		.attr("width", w)
-        .attr("height", h)
-        .attr("class", "container")
-		.attr("class", "container")
-        .style("background-color", "rgba(0,0,0,0.2)");
-		
-    var x = d3.scaleLinear()
-        .range([90, 700])
-        .domain([0, 3]);
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [d3.csv("data/acs.csv"),
+                    d3.json("data/wi_counties.topojson")
+                   ];
+    Promise.all(promises).then(callback);
 	
-	var innerRect = container.append("rect")
-        .datum(400)
-        .attr("width", function(d){
-            return d * 2;
-        }) 
-        .attr("height", function(d){
-            return d;
-        })
-		.attr("class", "innerRect")
-        .attr("x", 50)
-        .attr("y", 50)
-        .style("fill", "#FFFFFF");
-		
-	var dataArray = [10, 20, 30, 40, 50];
-
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
-	
-	//find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
-
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
-
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) //was 440, 95
-        .domain([0, 700000]); //was minPop, maxPop
-		
-				
-	var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
-
-    var circles = container.selectAll(".circles")
-        .data(cityPop)
-        .enter()
-        .append("circle")
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            return x(i);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-		.style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000");
-		
-				
-	var yAxis = d3.axisLeft(y);
-
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis);
-		
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations");
-		
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population) - 4;
-        });
-
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
-		
-		    //create format generator
-    var format = d3.format(",");
-
-    //Example 3.16 line 1...second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
-}
+	function callback(data){
+	  csvData = data[0];
+	  wi = data[1];
+      console.log(csvData);
+      console.log(wi);
+	  
+	  var wiCounties = topojson.feature(wi,wi.objects.cb_2015_wisconsin_county_20m);
+	  
+	  console.log(wiCounties);
+	  
+	  var counties = map.append("path")
+        .datum(wiCounties)
+        .attr("class", "counties")
+        .attr("d", path);
+    };
+};
